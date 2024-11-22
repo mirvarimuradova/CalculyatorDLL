4e4f2e9e68fd898cde5bd76ca15cd5b918a6ccdc
mirvarimuradova <mirvarimuradova7@gmail.com>
2024-11-22 16:14:35 +0400

    Add project files.

---
+++ CalculyatorDLL/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculyatorDLL
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Bölən 0 ola bilməz.");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ CalculyatorDLL/Program.cs
@@ -0,0 +1,11 @@
+﻿using CalculyatorDLL;
+
+Calculator calculator = new Calculator();
+
+double num1 = 20;
+double num2 = 5;
+
+Console.WriteLine("Toplama: " + calculator.Add(num1, num2));
+Console.WriteLine("Çıxma: " + calculator.Subtract(num1, num2));
+Console.WriteLine("Vurma: " + calculator.Multiply(num1, num2));
+Console.WriteLine("Bölmə: " + calculator.Divide(num1, num2));
